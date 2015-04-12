70ddaa39faeba67cb727cfdf8f6db3d5e3a62d31
jcgod413 <jcgod413@gmail.com>
2015-04-13 01:09:29 +0900

    바코드 생성 라이브러리 변경(zxing) 바코드 소스내에서 동적생성

--- UserApp/app/src/main/java/com/example/jaecheol/tab/Tab1.java
+++ UserApp/app/src/main/java/com/example/jaecheol/tab/Tab1.java
@@ -1,18 +1,25 @@
 package com.example.jaecheol.tab;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.example.jaecheol.tongs_v10.BarcodeGenerator;
 import com.example.jaecheol.tongs_v10.R;
+import com.google.zxing.BarcodeFormat;
 
 /**
  * Created by JaeCheol on 15. 4. 7..
  */
 public class Tab1 extends Fragment {
+
+    Bitmap barcode;
+    ImageView imageView;
 
     @Override
     public View onCreateView(LayoutInflater inflater,
@@ -20,6 +27,17 @@ public class Tab1 extends Fragment {
                              @Nullable Bundle SavedInstanceState)
     {
         View v = inflater.inflate(R.layout.tab_1, container, false);
+        imageView = (ImageView)v.findViewById(R.id.imageView);
+
+        BarcodeGenerator barcodeGenerator = new BarcodeGenerator();
+        try {
+            barcode = barcodeGenerator.encodeAsBitmap("Hello", BarcodeFormat.CODE_128, 600, 400);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        imageView.setImageBitmap(barcode);
+
         return v;
     }
 }
