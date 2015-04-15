caeb30a6acfe945c5e84dbddd53eda45e85eb616
Jun <sobu00@naver.com>
2015-04-15 17:16:01 +0900

    back up

---
+++ ManagerApp/uIAppTemplate/src/main/java/com/csform/android/uiapptemplate/SignUpActivitySecond.java
@@ -0,0 +1,70 @@
+package com.csform.android.uiapptemplate;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+public class SignUpActivitySecond extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_activity_second);
+
+        Button mBackButton = (Button)findViewById(R.id.sign_up_back_button);
+        mBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ActivityBack();
+
+            }
+        });
+        Button mNextButton = (Button)findViewById(R.id.sign_up_next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ActivityNext();
+
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_sign_up_activity_second, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    public void ActivityBack(){
+        this.finish();
+        overridePendingTransition(R.anim.slide_right, R.anim.slide_out_right);
+
+    }
+    public void ActivityNext(){
+        Intent intent = new Intent(this, SignUpActivityThird.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.slide_left, R.anim.slide_out_left);
+    }
+}
