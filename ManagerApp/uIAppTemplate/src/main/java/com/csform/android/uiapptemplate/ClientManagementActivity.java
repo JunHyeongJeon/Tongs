851512799267492ce314b57f1ddcffa8e5efe888
Jun <sobu00@naver.com>
2015-04-20 20:03:15 +0900

    server folder

--- ManagerApp/uIAppTemplate/src/main/java/com/csform/android/uiapptemplate/ClientManagementActivity.java
+++ ManagerApp/uIAppTemplate/src/main/java/com/csform/android/uiapptemplate/ClientManagementActivity.java
@@ -86,6 +86,7 @@ public class ClientManagementActivity extends ActionBarActivity {
         mClientAddButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 AddClient();
 
 
