56c8524bb094090503760a7bdc4718d8f405494c
Jun <sobu00@naver.com>
2015-05-23 01:49:12 +0900

    add tab bar

--- ManagerApp/managerapp/src/main/java/com/csform/android/managerapp/activity/PreviousClientListActivity.java
+++ ManagerApp/managerapp/src/main/java/com/csform/android/managerapp/activity/PreviousClientListActivity.java
@@ -5,7 +5,6 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.ViewGroup;
 
 import com.csform.android.managerapp.R;
 import com.csform.android.managerapp.SlidingTabsBasicFragment;
@@ -32,19 +31,16 @@ public class PreviousClientListActivity extends ActionBarActivity {
     }
 
     @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
+
 
 
 }
